8f3644af537707bd5696b251b2812ec6e81defa3
Andrew Gustafson <gus4000@gmail.com>
2020-12-19 17:35:29 +0000

    Use isSnapshot; add logging

--- core/src/main/scala/io/chrisdavenport/sbtmimaversioncheck/MimaVersionCheck.scala
+++ core/src/main/scala/io/chrisdavenport/sbtmimaversioncheck/MimaVersionCheck.scala
@@ -21,7 +21,7 @@ object MimaVersionCheck extends AutoPlugin {
       major: Int,
       minor: Int,
       patch: Int,
-      snapshotUsesLatestReleasedVersion: Boolean
+      isSnapshotOfLatestReleasedVersion: Boolean
   ): Set[(Int, Int, Int)] = {
     val majorVersions: List[Int] =
       if (major == 0 && minor == 0) List.empty[Int] // If 0.0.x do not check MiMa
@@ -33,7 +33,7 @@ object MimaVersionCheck extends AutoPlugin {
       if (minor == 0 && patch == 0) List.empty[Int]
       else if (currentMinVersion != minor) List(0)
       else {
-        val maxPatchValue = if (snapshotUsesLatestReleasedVersion) patch else patch - 1
+        val maxPatchValue = if (isSnapshotOfLatestReleasedVersion) patch else patch - 1
         Range(0, maxPatchValue).inclusive.toList
       }
 
@@ -45,14 +45,14 @@ object MimaVersionCheck extends AutoPlugin {
     versions.toSet
   }
 
-  def mimaVersions(version: String, snapshotUsesLatestReleasedVersion: Boolean): Set[String] =
+  def mimaVersions(version: String, isSnapshotOfLatestReleasedVersion: Boolean): Set[String] =
     VersionNumber(version) match {
-      case VersionNumber(Seq(major, minor, patch, _*), _, extras) =>
+      case VersionNumber(Seq(major, minor, patch, _*), _, _) =>
         semverBinCompatVersions(
           major.toInt,
           minor.toInt,
           patch.toInt,
-          extras.exists(_.endsWith("-SNAPSHOT")) && snapshotUsesLatestReleasedVersion
+          isSnapshotOfLatestReleasedVersion
         ).map { case (maj, min, pat) => maj.toString + "." + min.toString + "." + pat.toString }
       case _ =>
         Set.empty[String]
@@ -64,22 +64,36 @@ object MimaVersionCheck extends AutoPlugin {
     mimaVersionCheckSnapshotUsesLatestReleasedVersion := false,
   )
 
-  override def projectSettings: Seq[Def.Setting[_]] = List(
-    mimaFailOnNoPrevious := false,
-    mimaFailOnProblem :=
-      mimaVersions(version.value, mimaVersionCheckSnapshotUsesLatestReleasedVersion.value).toList.nonEmpty,
-    mimaPreviousArtifacts :=
-      (mimaVersions(version.value, mimaVersionCheckSnapshotUsesLatestReleasedVersion.value) ++
-        mimaVersionCheckExtraVersions.value)
-        .diff(mimaVersionCheckExcludedVersions.value)
-        .map { v =>
-          val moduleN = if (sbtPlugin.value) {
-            moduleName.value + "_" + scalaBinaryVersion.value.toString + "_" + sbtBinaryVersion.value.toString
-          } else {
-            moduleName.value + "_" + scalaBinaryVersion.value.toString
+  override def projectSettings: Seq[Def.Setting[_]] = {
+    List(
+      mimaFailOnNoPrevious := false,
+      mimaFailOnProblem := mimaVersions(
+        version.value,
+        isSnapshot.value && mimaVersionCheckSnapshotUsesLatestReleasedVersion.value
+      ).toList.nonEmpty,
+      mimaPreviousArtifacts := {
+        val fullVersionSet =
+          (mimaVersions(
+            version.value,
+            isSnapshot.value && mimaVersionCheckSnapshotUsesLatestReleasedVersion.value
+          ) ++ mimaVersionCheckExtraVersions.value)
+            .diff(mimaVersionCheckExcludedVersions.value)
+        val msg =
+          if (fullVersionSet.nonEmpty)
+            s"Checking against versions ${fullVersionSet.toList.sorted.mkString(", ")}"
+          else "No versions to check"
+        sLog.value.info(s"MiMa: ${moduleName.value} - $msg")
+        fullVersionSet
+          .map { v =>
+            val moduleN = if (sbtPlugin.value) {
+              moduleName.value + "_" + scalaBinaryVersion.value + "_" + sbtBinaryVersion.value
+            } else {
+              moduleName.value + "_" + scalaBinaryVersion.value
+            }
+            organization.value % moduleN % v
           }
-          organization.value % moduleN % v
-        }
-  )
+      }
+    )
+  }
 
 }
